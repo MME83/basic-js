05a3d28e1e9d40b24c1a380fdf62f04c5cc368dd
Mykola Moroz <moroz.mykola@gmail.com>
2020-09-28 01:51:29 +0300

    feat: make task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam( members ) {
+  
   // remove line with error and write your code here
+
+  let teamName = '';
+  
+  if (typeof(members) != 'object' || !members) {
+    return false;
+  } else {
+
+    for (let i = 0; i < members.length; i++) {
+      if (typeof(members[i]) == 'string') {
+        for (let j = 0; j < members[i].length; j++) {
+          if (members[i][j].match(/[A-Z,a-z]/)) {
+            teamName += members[i][j];
+            break;
+          }
+        } 
+      }
+    }
+
+    return teamName.toUpperCase().split('').sort().join('');
+  }
 };
