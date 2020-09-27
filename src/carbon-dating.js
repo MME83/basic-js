0bf5205d6d269c51dc2ff3993f03a1b72bbb223a
Mykola Moroz <moroz.mykola@gmail.com>
2020-09-27 23:14:20 +0300

    feat: make second task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  
+  const k = (Math.LN2 * (10**4)) / HALF_LIFE_PERIOD;
+
+  if (typeof(sampleActivity) == "string" && Number.isInteger(+sampleActivity) && +sampleActivity > 0 && +sampleActivity < 16) {
+    const t = (Math.log(MODERN_ACTIVITY / +sampleActivity) / k) * 10**4;
+    return Math.round(t);
+  } else {
+    return false;
+  }
 };
